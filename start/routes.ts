762330e267c0af5d4461ab575b3e13a021177cf4
Kamil Ramocki <kamil.ramocki@gmail.com>
2025-01-23 20:00:44 +0100

    fix: change counting method

--- start/routes.ts
+++ start/routes.ts
@@ -1,4 +1,46 @@
+import swagger from "#config/swagger";
 import router from "@adonisjs/core/services/router";
+import AutoSwagger from "adonis-autoswagger";
+import { middleware } from "./kernel.js";
 
+
+const BlocksController = () => import("#controllers/blocks_controller");
+const EventController  = () => import ("#controllers/events_controller");
 const ParticipantsController = () => import("#controllers/participants_controller");
-router.resource("/api/v1/participants", ParticipantsController);
+const AuthController = () => import("#controllers/auth_controller");
+const PermissionsController = () => import("#controllers/permissions_controller");
+const AdminsController = () => import("#controllers/admins_controller");
+const EmailsController = () => import("#controllers/emails_controller");
+
+router.get("/swagger", async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger);
+});
+router.get("/docs", async () => {
+  return AutoSwagger.default.scalar("/swagger");
+});
+
+router
+  .group(() => {
+    router.resource("emails", EmailsController).apiOnly();
+  })
+  .prefix("api/v1/events/:event_id");
+
+router
+  .group(() => {
+    router.resource("participants", ParticipantsController).apiOnly();
+    router.resource("permissions", PermissionsController);
+    router.resource("admins", AdminsController);
+    router.resource("events", EventController);
+    router.resource("blocks", BlocksController);
+
+
+
+    router
+      .group(() => {
+        router.post("login", [AuthController, "login"]);
+        router.post("register", [AuthController, "register"]);
+        router.get("me", [AuthController, "me"]).use(middleware.auth());
+      })
+      .prefix("auth");
+  })
+  .prefix("api/v1");
