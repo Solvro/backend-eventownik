76a82fac4ca5a32ada2b2cc29129eb2b57f45f4d
Kamil Ramocki <kamil.ramocki@gmail.com>
2025-02-25 19:08:00 +0100

    fix: pull request changes

--- app/controllers/forms_controller.ts
+++ app/controllers/forms_controller.ts
@@ -2,7 +2,11 @@ import type { HttpContext } from "@adonisjs/core/http";
 
 import Event from "#models/event";
 import Form from "#models/form";
-import { createFormValidator, updateFormValidator } from "#validators/form";
+import {
+  createFormValidator,
+  filledFieldsValidator,
+  updateFormValidator,
+} from "#validators/form";
 
 export default class FormsController {
   /**
@@ -45,12 +49,12 @@ export default class FormsController {
       attributes.reduce(
         (acc, attribute) => {
           acc[attribute.id] = {
-            is_required: attribute.isRequired === true || false,
-            is_editable: true,
+            is_required: attribute.isRequired,
+            is_editable: attribute.isEditable,
           };
           return acc;
         },
-        {} as Record<number, { is_required: boolean; is_editable: boolean }>,
+        {} as Record<number, { is_required?: boolean; is_editable?: boolean }>,
       ),
     );
 
@@ -108,12 +112,15 @@ export default class FormsController {
         attributes.reduce(
           (acc, attribute) => {
             acc[attribute.id] = {
-              is_required: attribute.isRequired === true || false,
-              is_editable: true,
+              is_required: attribute.isRequired,
+              is_editable: attribute.isEditable,
             };
             return acc;
           },
-          {} as Record<number, { is_required: boolean; is_editable: boolean }>,
+          {} as Record<
+            number,
+            { is_required?: boolean; is_editable?: boolean }
+          >,
         ),
       );
     }
@@ -160,6 +167,8 @@ export default class FormsController {
   public async requiredFields({ params, request, response }: HttpContext) {
     const formId = +params.id;
 
+    const { filledFields } = await request.validateUsing(filledFieldsValidator);
+
     const form = await Form.query()
       .where("id", formId)
       .preload("attributes", async (query) => {
@@ -167,14 +176,8 @@ export default class FormsController {
       })
       .firstOrFail();
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const { filledFields } = request.only(["filledFields"]);
-
-    const requiredAttributes = form.attributes.filter(
-      (attribute) => attribute.$extras.pivot_is_required === true,
-    );
-
-    const missingRequiredFields = requiredAttributes
+    const missingRequiredFields = form.attributes
+      .filter((attribute) => attribute.$extras.pivot_is_required === true)
       .filter(
         (attribute) =>
           !Object.prototype.hasOwnProperty.call(filledFields, attribute.name),
